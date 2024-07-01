abf759643fad48d2f448222c9c76dd6f2080ec7a
Rehman-codes <rehman.programs@gmail.com>
2024-07-01 15:18:18 +0500

    skills section updated

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,8 @@
 import './skills.css'
+import cpp from '../../assets/skillsLogos/c.png'; 
+import figma from '../../assets/skillsLogos/figma.png'; 
+import firebase from '../../assets/skillsLogos/firebase.png'; 
+import js from '../../assets/skillsLogos/js.png'; 
 
 function Skills() {
 
@@ -8,11 +12,14 @@ function Skills() {
         <h1>Skills</h1>
 
         <section id="skills">
-            <div className='skill'>
-             
+            <div id='skill'>
+            <img src={cpp} alt="Image 1" />
+            <img src={js} alt="Image 1" />
+            <img src={figma} alt="Image 1" />
+            <img src={firebase} alt="Image 1" />
             </div>
         </section>
-        
+
         </>
     )
 }
