06bea684a14b516cd48fc20616f99614b32065fb
Rehman-codes <rehman.programs@gmail.com>
2024-07-01 12:35:56 +0500

    projects section logic complete

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import './projects.css';
+import { useState, useEffect } from 'react';
+
+function Projects() {
+    const [allReposData, setAllReposData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        try {
+            const response = await fetch('https://api.github.com/users/Rehman-codes/repos');
+            const data = await response.json();
+            setAllReposData(data);
+        } catch (error) {
+            console.error('error fetching data', error);
+        }
+    }
+
+    const allRepos = allReposData.map(repo => {
+        return (
+        <div key={repo.id} className="card">
+            <h2>{repo.name || 'Title'}</h2>
+            <p>{repo.description || 'Description'}</p>
+            <button onClick={() => window.location.href = repo.html_url}>Github</button>
+            <button onClick={() => window.location.href = repo.homepage}>Preview</button>
+        </div>
+        )
+});
+
+    return (
+        <>
+            <h1>Projects</h1>
+            <section id="projects">
+                {allRepos}
+            </section>
+        </>
+    );
+}
+
+export default Projects;
