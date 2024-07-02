84c657ef2845b2c9dd3aef35c5df6e13e0785545
Rehman-codes <rehman.programs@gmail.com>
2024-07-02 14:33:40 +0500

    UI update

--- src/components/Top/top.jsx
+++ src/components/Top/top.jsx
@@ -3,16 +3,16 @@ import gitHub from '../../assets/github.png'
 import LinkedIn from '../../assets/LinkedIn.png'
 import Instagram from '../../assets/instagram.png'
 
-function Top(){
+function Top() {
 
 
     return (
         <>
-        <section id='top'>
-            <a href="https://github.com/Rehman-codes" target="_blank"><img src={gitHub} alt="Github" /></a>
-            <a href="https://www.linkedin.com/in/rehman-chaudhary" target="_blank"><img src={LinkedIn} alt="LinkedIn" /></a>
-            <a href="https://www.instagram.com/8Bits.solutions/" target="_blank"><img src={Instagram} alt="Instagram" /></a>
-        </section>
+            <section id='top'>
+                    <a href="https://github.com/Rehman-codes" target="_blank"><img src={gitHub} alt="Github" /></a>
+                    <a href="https://www.linkedin.com/in/rehman-chaudhary" target="_blank"><img src={LinkedIn} alt="LinkedIn" /></a>
+                    <a href="https://www.instagram.com/8Bits.solutions/" target="_blank"><img src={Instagram} alt="Instagram" /></a>
+            </section>
         </>
     )
 }
