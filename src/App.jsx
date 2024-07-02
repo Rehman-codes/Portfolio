0b4f90bf91499e19fbd186bf5bf4f612c6a73e77
Rehman-codes <rehman.programs@gmail.com>
2024-07-02 13:32:28 +0500

    added top component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Projects from './components/Projects/Projects.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import Skills from './components/Skills/Skills.jsx';
 import Intro from './components/Intro/Introduction.jsx';
+import Top from './components/Top/top.jsx';
 
 function App() {
 
   return (
     <>
+      <Top/>
       <Intro />
       <Projects/>
       <Skills/>
