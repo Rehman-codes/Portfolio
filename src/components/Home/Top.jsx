c23712f4fdb18d21014084e710caf78532d7abb2
Rehman-codes <abdurrehman.swe@gmail.com>
2025-06-07 18:26:29 +0500

    Responsive Home page

--- src/components/Home/Top.jsx
+++ src/components/Home/Top.jsx
@@ -7,7 +7,7 @@ export default function Top() {
             <div className="h-12 w-12 ml-4 group">
                 <img src="/dp.png" alt="profile pic" className="border-[0.5px] border-black cursor-pointer h-full w-full object-cover rounded-full transition-transform duration-250 ease-in-out group-hover:scale-300 group-hover:translate-x-10 group-hover:translate-y-10" />
             </div>
-            <a href="https://1drv.ms/w/c/aca42ed92d4d30f1/ER6pWwsNyKRGmxpawGeEzRYB-0K7kOy5yF9Cn_hYYpe3uQ" target="_blank" className="border-[0.5px] border-black flex flex-row items-center justify-center gap-2 mr-8 p-4 bg-white text-black rounded-3xl h-[60%] w-[9%] cursor-pointer"><Download size={24} /> Resume</a>
+            <a href="https://1drv.ms/w/c/aca42ed92d4d30f1/ER6pWwsNyKRGmxpawGeEzRYB-0K7kOy5yF9Cn_hYYpe3uQ" target="_blank" className="border-[0.5px] border-black flex flex-row items-center justify-center gap-2 mr-8 p-2 md:p-4 bg-white text-black rounded-3xl h-[60%] w-[30%] md:w-[9%] cursor-pointer"><Download size={24} /> Resume</a>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,32 +4,31 @@ import Bottom from "../components/Home/Bottom";
 
 export default function Home() {
     return (
-        <section className="w-screen h-screen bg-black flex flex-col md:flex-row relative">
+        <section className="w-screen h-screen bg-black flex flex-col md:flex-row justify-center items-center relative">
 
             {/* Intro Section */}
-            <div id="intro" className="w-[50%] flex flex-col justify-center items-center">
-
-                <div className="w-[75%]">
-                    <h1 className="ml-[10%] text-white text-9xl font-bold">Abdur Rehman</h1>
+            <div id="intro" className="w-full md:w-[50%] flex flex-col justify-center items-center px-4 md:px-0">
+                <div className="w-full md:w-[75%] flex flex-col text-center md:text-left">
+                    <h1 id="name" className="text-white text-5xl md:text-9xl font-bold">
+                        Abdur Rehman
+                    </h1>
+                    <p id="description" className="mt-2 text-white font-thin text-base md:text-lg">
+                        Software Engineer | Full Stack Web Developer
+                    </p>
                 </div>
-
-                <div className="w-[75%]">
-                    <p className="mt-[1%] ml-[11%] text-white font-thin text-lg">Software Engineer | Full Stack Web Developer</p>
-                </div>
-
             </div>
 
             {/* Spline 3D Model */}
-            <div className="w-[50%] h-full relative">
+            <div className="w-full md:w-[50%] h-[50%] md:h-full flex justify-center items-center relative">
                 <Spline
                     scene="https://prod.spline.design/UgiBiSGLOhQqrvaP/scene.splinecode"
-                    className="flex justify-center items-center"
+                    className="w-full h-full"
                 />
+                <div className="absolute bottom-0 right-0 w-40 h-16 bg-black"></div>
             </div>
 
             {/* Top Navbar */}
             <Top />
-
 
             {/* Bottom Navbar */}
             <Bottom />
